3183fdada6900e89920e1e8d422d65d22c851577
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-07-08 18:55:04 +0300

    Update render_client.sh

--- lab_setup/render_client.sh
+++ lab_setup/render_client.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "Setting up server on Render..."
-cd server
-pip install -r requirements.txt
-gunicorn --bind 0.0.0.0:$PORT c2_server:app
+echo "Setting up client on Render..."
+cd client
+python client_web.py
